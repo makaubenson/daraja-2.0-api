ee5cd2fe3517e128c6e6b7361d8d2f8997ef1f03
Benson <bensonmakau2000@gmail.com>
2022-05-27 23:23:27 +0300

    updated creds 2

--- stk_initiate.php
+++ stk_initiate.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
     for developer/test accounts, this money will be reversed automatically by midnight.
   */
   
-  $PartyA = $_POST['254790333257']; // This is your phone number, 
+  $PartyA = $_POST['254758413462']; // This is your phone number, 
   $AccountReference = '2255';
   $TransactionDesc = 'Test Payment';
   $Amount = $_POST['amount'];;
